a4901da081b907335ade2a2561e1861c3fc32067
Maiara Evangelista <maiaraevangelista933@gmail.com>
2020-11-29 16:31:30 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Fixação3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+
+          Console.WriteLine("Digite o nome do produto: ");
+          string nomeDoProduto = Console.ReadLine();
+
+          Console.WriteLine("Insira a quantidade desejada: ");
+          int quantidade = int.Parse(Console.ReadLine());
+
+          Console.WriteLine("Insira o valor do produto: ");
+          float preco = float.Parse(Console.ReadLine());
+
+            TotalAPagar(nomeDoProduto, quantidade, preco);
+              float TotalAPagar(string nome, int quantidade, float preco){
+              float total = quantidade * preco;
+              float desconto;
+               
+              if(quantidade<=5){
+                  desconto = 0.02f;
+              } else if(quantidade <=10){
+                  desconto = 0.03f;
+              } else {
+                  desconto = 0.05f;
+              }
+
+              float valorDesconto = total * desconto;
+              float totalDesconto = total - valorDesconto;
+
+              Console.WriteLine($"Sua compra ficou em R${total}");
+              Console.WriteLine($"Você vai receber R${valorDesconto} de desconto");
+              Console.WriteLine($"Sua compra com desconto ficou em R$ {totalDesconto}");
+
+               return total;
+          }
+        }
+    }
+}
